6c0dc10617895837f5f5bd9a063ed4d0724929de
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-08-05 14:31:34 +0800

    update build

---
+++ public/build/assets/use-page-props-B7AgGzm3.js
@@ -0,0 +1,1 @@
+import{J as r}from"./app-DyQmic5P.js";function s(){const{props:o}=r();return o}export{s as u};
--- public/build/assets/use-page-props-Dq-CPdva.js
+++
@@ -1,1 +0,0 @@
-import{J as r}from"./app-mdqh7xPi.js";function s(){const{props:o}=r();return o}export{s as u};
